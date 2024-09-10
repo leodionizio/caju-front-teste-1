bb3ad194a5b10d2f8259dab9b22ad7f44cf53351
leoadionizio <leonardo.dionizio@kovi.com.br>
2024-09-10 17:50:46 -0300

    feat: add providers and root components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import Router from "~/router";
 import { Header } from "./components/Header";
+import { RegistrationsProvider } from "./contexts/registrationsContext";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
+import { ConfirmationDialogProvider } from "./contexts/confirmationDialogContext";
+import { ConfirmationModal } from "./components/Dialogs/ConfirmationDialog";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { LoaderProvider } from "./contexts/loaderContext";
+import { Loader } from "./components/Loader";
 
 function App() {
   return (
     <>
-      <Header>
-        <h1>Caju Front Teste</h1>
-      </Header>
-      <Router />
+      <ThemeProvider theme={theme}>
+        <Header>
+          <h1>Caju Front Teste</h1>
+        </Header>
+        <ConfirmationDialogProvider>
+          <LoaderProvider>
+            <RegistrationsProvider>
+              <Loader>
+                <Router />
+                <ConfirmationModal />
+                <ToastContainer />
+              </Loader>
+            </RegistrationsProvider>
+          </LoaderProvider>
+        </ConfirmationDialogProvider>
+      </ThemeProvider>
     </>
   );
 }
