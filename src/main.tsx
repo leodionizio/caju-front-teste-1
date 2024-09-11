f4836528590e999e9edbf1b7c3b6ea95e9ad8be2
Leonardo Dionizio <leonardo.dionizio@kovi.com.br>
2024-09-11 17:45:37 -0300

    chore: add logrocket to error monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import './styles/index.css'
+import "./styles/index.css";
+import LogRocket from "logrocket";
+
+LogRocket.init("en9sos/caju-front-test");
+
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
